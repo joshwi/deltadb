25d627150b123f494dcfcaa4879c94d6d857e81c
Josh Williams <josh.t.williams@outlook.com>
2021-11-19 15:22:35 -0800

    testing master build

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo docker create deltadb
+sudo docker run -d --net deltadb --env-file .env  --name neo4j -p 7474:7474 -p 7687:7687 -v /Users/josh/Desktop/repos/nfldb-backup:/var/lib/neo4j/import jshwilliams/neo4j
+sudo docker run -d --net deltadb --env-file .env  --name redis -p 6379:6379 jshwilliams/redis
+sudo docker run -d --net deltadb --env-file .env  --name deltadb-api -p 5000:5000 jshwilliams/deltadb-api
+sudo docker run -d --net deltadb --env-file .env  --name deltadb-ui -p 80:80 jshwilliams/deltadb-ui
+sudo docker run -d --net deltadb --env-file .env  --name deltadb-collector -v /Users/josh/Desktop/repos/nfldb-backup:/app/repos jshwilliams/deltadb-collector
