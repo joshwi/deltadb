ab20cc745d81e7c42ee26036de1520e5bb9174d0
Josh <josh.t.williams@outlook.com>
2022-01-12 08:09:08 +0000

    putting things together

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Change to /app directory
+mkdir repos
 cd app
 # Make sure all downloads and repositories are removed
 rm neo4j/*.zip
@@ -7,13 +8,14 @@ rm -rf neo4j/deltadb
 rm -rf neo4j/__MACOSX
 rm -rf deltadb-ui
 rm -rf deltadb-api
-rm -rf deltadb-backup
 rm -rf go-scripts
 # Reclone repositories to get latest version
 git clone https://github.com/joshwi/deltadb-ui.git
 git clone https://github.com/joshwi/deltadb-api.git
-git clone https://github.com/joshwi/deltadb-backup.git
 git clone https://github.com/joshwi/go-scripts.git
+cd ../repos
+rm -rf deltadb-backup
+git clone https://github.com/joshwi/deltadb-backup.git
 cd ..
 # Copy .env to all subdirectories
 cp .env app/deltadb-api/app/.env
@@ -21,7 +23,7 @@ cp .env app/deltadb-ui/.env
 cp .env app/neo4j/.env
 cp .env app/go-scripts/.env
 # Load DB Backups to /neo4j directory
-cp app/deltadb-backup/nfl.zip app/neo4j/nfl.zip
+cp repos/deltadb-backup/*.zip app/neo4j/
 cd app/neo4j
-unzip nfl.zip -d deltadb/
+unzip "*.zip" -d deltadb/
 rm -rf __MACOSX
